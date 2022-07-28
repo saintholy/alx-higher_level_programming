576a8f91a7f00105e154b6169548c29d6259cc7a
saintholy <ahenkorahhasiamah@gmail.com>
2022-07-27 17:59:47 -0700

    Task 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
-Class Rectangle:
-    """ A rectangle class of a plain with length and breath as it's sides """
+class Rectangle:
+
+    """ A rectangle class of a plain with 
+        length and breath as it's sides 
+    """
     pass
