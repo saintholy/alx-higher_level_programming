01cbe3440c8db6a108bf602971ad2cb03539822f
saintholy <ahenkorahhasiamah@gmail.com>
2022-10-03 22:27:46 -0700

    JavaScript file 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < myArray.length; i++) {
-  console.log(myArray[i]);
+const msg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(msg[i]);
 }
