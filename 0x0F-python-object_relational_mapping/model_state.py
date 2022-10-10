da2027c2799f43edafc8fd710797fcc6e8aa7a6e
saintholy <39052604+saintholy@users.noreply.github.com>
2022-10-10 08:39:57 -0700

    Task 6
    Python file that contains the class definition of a State and an instance
    """

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
 """
-Script that takes in the name of a state as an argument and lists
-all cities of that state, using the database
+Python file that contains the class definition of a State and an instance
 """
-import MySQLdb
-from sys import argv
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-# The code should not be executed when imported
-if __name__ == '__main__':
-    # make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+Base = declarative_base()
 
-    cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name FROM cities\
-                INNER JOIN states ON cities.state_id = states.id\
-                WHERE states.name = %s", [argv[4]])
 
-    rows = cur.fetchall()
-    j = []
-    for i in rows:
-        j.append(i[1])
-    print(", ".join(j))
-
-    # Clean up process
-    cur.close()
-    db.close()
+class State(Base):
+    """
+    State class that inherits from Base
+    Attributes:
+        id: Id state
+        name: Name of state
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
